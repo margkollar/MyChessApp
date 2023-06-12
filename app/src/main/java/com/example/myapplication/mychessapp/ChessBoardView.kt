1622a76a3096f6c1d6c021d5e3bf89fddb91ac56
Margarita <margaritakollarou13@gmail.com>
2023-06-12 12:35:15 +0300

    Build ui & view class

---
+++ app/src/main/java/com/example/myapplication/mychessapp/ChessBoardView.kt
@@ -0,0 +1,11 @@
+package com.example.myapplication.mychessapp
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.util.AttributeSet
+import android.view.MotionEvent
+import android.view.View
+
+class ChessBoardView(context: Context?, attrs: AttributeSet?) : View(context, attrs) {
+}
